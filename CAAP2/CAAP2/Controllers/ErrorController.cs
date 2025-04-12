0288ebbe2503d8f3dc743590554f20381251e2b2
paolis <mramirez00473@ufide.ac.cr>
2025-04-11 22:56:49 -0600

    actualizado

---
+++ CAAP2/CAAP2/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace CAAP2.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
